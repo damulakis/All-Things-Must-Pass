bda255c37f1fd2b991f05f09b788b3288a6662f9
damulakis <eduardalakis@gmail.com>
2026-06-18 22:30:41 -0300

    Main

---
+++ app.js
@@ -0,0 +1,10 @@
+const  menuButton = document.getElementById("dropbtn");
+const  menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});   
